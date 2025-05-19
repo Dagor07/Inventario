3c955db1061eb8372a7bd83674a0aef18cff0ea0
Dagor07 <danielgonzaortiz@gmail.com>
2025-05-19 13:31:30 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -5,13 +5,13 @@ libros = []
 contador_id = 1
 prestamos_por_persona = {}
 
-def generar_id():
+def generate_id():
     global contador_id
     nuevo_id = contador_id
     contador_id += 1
     return nuevo_id
 
-def validar_entrada(texto, tipo=int, condiciones=None):
+def validate_entry(texto, tipo=int, condiciones=None):
     while True:
         valor = input(texto)
         if not valor.strip():
@@ -27,7 +27,7 @@ def validar_entrada(texto, tipo=int, condiciones=None):
         except:
             print("Formato inválido.")
 
-def registrar_libro():
+def register_book():
     titulo = input("Título: ").strip()
     autor = input("Autor: ").strip()
     año = validar_entrada("Año de publicación: ", int, lambda x: 1500 <= x <= datetime.date.today().year)
@@ -48,7 +48,7 @@ def registrar_libro():
 
 
 
-def prestar_libro():
+def lend_book():
     id_libro = validar_entrada("ID del libro a prestar: ")
     libro = next((l for l in libros if l["id"] == id_libro), None)
     if not libro:
@@ -68,3 +68,56 @@ def prestar_libro():
     }
     prestamos_por_persona[nombre] = prestamos_por_persona.get(nombre, 0) + 1
     print(" Libro prestado correctamente.")
+
+
+def show_books(books, filter = None):
+    print(f"{"****Listado de libros****"}".rjust(60))
+    print("*"*115)
+    print(f"\n{"ID".center(10)}|{"Titulo del libro".center(25)}|{"Autor".center(25)}|{"Año de publicacion".center(20)}|{"Categoria".center(15)}|{"Estado".center(15)}")
+    print("*"*115)
+    found = False
+    for book in books:
+        if filter is None or book["estado"] == filter:
+            found = True
+
+            id = str(book["id"])
+            title = str(book["titulo"]).capitalize()
+            author = str(book["autor"]).title()
+            year = str(book["año"])
+            category = str(book["categoria"]).capitalize()
+            condition = str(book["estado"]).capitalize()
+            print(f"{id.center(10)}|{title.center(25)}|{author.center(25)}|{year.center(20)}|{category.center(15)}|{condition.center(15)}")
+            print("-"*115)
+    if not found and filter:
+        print(f"\nNo se encontraron libros con estado '{filter}'.\n")
+    elif not found and filter is None:
+         print("No hay libros registrados.")
+
+
+
+
+def search_book(books, search):
+    
+    print("*"*115)
+    print(f"\n{"ID".center(10)}|{"Titulo del libro".center(25)}|{"Autor".center(25)}|{"Año".center(20)}|{"Categoria".center(15)}|{"Estado".center(15)}")
+    print("*"*115)
+    found  = False
+    search = search.lower()
+    for book in books:
+        title = book["titulo"]
+        author = book["autor"]
+        category = book["categoria"]
+        if search in title or search in author or search in category:
+            found  = True
+            id = str(book["id"])
+            title = str(book["titulo"]).capitalize()
+            author = str(book["autor"]).title()
+            year = str(book["año"])
+            category = str(book["categoria"]).capitalize()
+            condition = str(book["estado"]).capitalize()
+          
+            print(f"{id.center(10)}|{title.center(25)}|{author.center(25)}|{year.center(20)}|{category.center(15)}|{condition.center(15)}")
+            print("-"*115)
+    if not found:
+        print("No se encontraron libros que coincidan con la búsqueda.")
+        
